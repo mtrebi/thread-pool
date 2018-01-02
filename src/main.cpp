ef19f849963e502824437a1941bb119cd2bec032
Tiaan Louw <tiaan.louw@olx.com>
2018-01-02 11:22:56 +0100

    Replace _sleep with std::this_thread_sleep_for which is more portable

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,36 @@
 #include <iostream>
 #include <random>
+#include <thread>
 
 #include "ThreadPool.h"
 
 std::random_device rd;
 std::mt19937 mt(rd());
-std::uniform_real_distribution<double> dist(0, 1000);
-auto random = std::bind(dist, mt);
+std::uniform_int_distribution<int> dist(-1000, 1000);
+auto rnd = std::bind(dist, mt);
 
+
+void simulate_hard_computation() {
+  std::this_thread::sleep_for(std::chrono::milliseconds(2000 + rnd()));
+}
 
 // Simple function that adds multiplies two numbers and prints the result
 void multiply(const int a, const int b) {
-  // Simulate hard computation
-  _sleep(2000 + random());
+  simulate_hard_computation();
   const int res = a * b;
   std::cout << a << " * " << b << " = " << res << std::endl;
 }
 
-
 // Same as before but now we have an output parameter
 void multiply_output(int & out, const int a, const int b) {
-  // Simulate hard computation
-  _sleep(2000 + random());
+  simulate_hard_computation();
   out = a * b;
   std::cout << a << " * " << b << " = " << out << std::endl;
 }
 
 // Same as before but now we have an output parameter
 int multiply_return(const int a, const int b) {
-  // Simulate hard computation
-  _sleep(2000 + random());
+  simulate_hard_computation();
   const int res = a * b;
   std::cout << a << " * " << b << " = " << res << std::endl;
   return res;
